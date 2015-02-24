4ca8586b24fbe0ee96f2860e6c27e53cbd2bd17d
Piotr Maliński <riklaunim@gmail.com>
2015-02-24 20:30:34 +0100

    Add initial pozytywnie_facebook example project.

---
+++ pozytywnie_facebook/facebook_project/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'facebook_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
